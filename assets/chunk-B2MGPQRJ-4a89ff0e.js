03b9cb217ba5393538b628165d66af6aa0d069b9
SiracencoSerghei <SiracencoSerghei@users.noreply.github.com>
2023-06-19 13:07:04 +0000

    deploy: 86eae70167e0b70c114e9e57896bc21e8889e9d7

---
+++ assets/chunk-B2MGPQRJ-4a89ff0e.js
@@ -0,0 +1,1 @@
+import{f as i,g as t,o as c,j as r,c as o,h as m}from"./index-b7de7892.js";var g=i(function(a,s){const e=t("Heading",a),{className:f,...n}=c(a);return r.jsx(o.h2,{ref:s,className:m("chakra-heading",a.className),...n,__css:e})});g.displayName="Heading";export{g as H};
